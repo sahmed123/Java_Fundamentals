faf52822ba88256d19cf819a646855510a2d86a8
sahmed123 <sahmed123@hotmail.com>
2021-01-05 21:34:44 -0800

    Completed Exercise 05

--- src/labs_examples/datatypes_operators/labs/Exercise_05.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_05.java
@@ -13,13 +13,37 @@ class LogicalOperators {
         // example of "OR"
         boolean a = true;
         boolean b = false;
+        boolean c = true;
+
+        //Example of OR
         if (a | b){
             System.out.println("a or b is true");
         }
 
-        // write your code below
+        // Example of AND
+        if(a & c){
+            System.out.println("both a and c are true");
+        }
 
+        //Example of &&
+        if(a == true && c == true){
+            System.out.println("short-circuit && evaluates to true");
+        }
+
+        //Example of ||
+        if(a || b){
+            System.out.println("One of a or b are true");
+        }
+
+        //Example of ^
+        if(a ^ b){
+            System.out.println("One of a or b is true, but not both of them");
+        }
+
+        //Example of !
+        if(a != b){
+            System.out.println("a and b are not the same value");
+        }
     }
 
 }
-
