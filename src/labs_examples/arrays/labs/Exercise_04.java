931d48639ddd8806b3e91f4972c74e2a0fdc0a95
sahmed123 <sahmed123@hotmail.com>
2021-01-07 22:11:10 -0800

    WIP

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,13 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main (String[] args){
+        int[][] twoDimsTwoFurious = new int[3][];
+        //Not quite certain of how to populate this, need to look up info on irregular arrays
+
+        //for(int[] i: twoDimsTwoFurious)
+          //  for(int[] j: twoDimsTwoFurious)
+            //System.out.println(twoDimsTwoFurious);
+    }
 
 }
