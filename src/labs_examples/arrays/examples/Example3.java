69897277a84f44066922597c3e84139a964942b6
PCR-Admin <sahmed123@hotmail.com>
2021-01-08 13:31:01 -0800

    Added some info

--- src/labs_examples/arrays/examples/Example3.java
+++ src/labs_examples/arrays/examples/Example3.java
@@ -11,8 +11,10 @@ class MinMax2 {
 
         min = max = nums[0];
         for(int i=1; i < 10; i++) {
-            if(nums[i] < min) min = nums[i];
-            if(nums[i] > max) max = nums[i];
+            if(nums[i] < min)
+                min = nums[i];
+            if(nums[i] > max)
+                max = nums[i];
         }
 
         System.out.println("Min and max: " + min + " " + max);
