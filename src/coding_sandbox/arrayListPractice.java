3d61dd63fd125dad7fb10d1232fac45535b4169a
PCR-Admin <sahmed123@hotmail.com>
2021-01-07 19:56:34 -0800

    arrayList Practice

--- src/coding_sandbox/arrayListPractice.java
+++ src/coding_sandbox/arrayListPractice.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 public class arrayListPractice {
     public static void main (String[] args){
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<String>();
 
         list.add("Hello");
-        list.add("Dog");
-        list.add("Yoyoyo");
+        list.add("World");
+        list.add("Check");
+        list.add("This");
+        list.add("Out!");
+
         //list.clear();
+
+        String val = list.get(2);
+        //System.out.println(val);
+
+        boolean check = list.contains("Dog");
+        //System.out.println(check);
 
         if(!list.isEmpty()){
             for(String s: list){
