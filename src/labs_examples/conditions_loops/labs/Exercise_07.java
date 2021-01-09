12ef4b7f03adb11774e5fc37975c4f0d397d808c
PCR-Admin <sahmed123@hotmail.com>
2021-01-08 16:06:01 -0800

    Completed

--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -14,8 +14,9 @@ package labs_examples.conditions_loops.labs;
  */
 
 /**   Want to have a string of vowels as a reference to check against --> "aeiou"
- *    Use length() to create a loop max var = to length of the word being passed in
+ *    Use length() to create a loop max var = to length of the word being passed in, go through each letter
  *    Use charAt() to pull the char value at a given location; use with loop value to log
+ *    Check this char against the vowel string. If the char is in the string, then we've found the first vowel
  *    Use indexOf() to return the position of the first match of what you're checking against
  *
  *    Iterate through an input string checking if the character at a given position matches ANY of the characters
@@ -28,23 +29,26 @@ public class Exercise_07 {
     static String vowels = "aeiou";
 
     public static void main (String[] args){
-        findVowel("dog");
+        findVowel("caribou");
     }
 
     public static void findVowel(String input){
 
-        int i = 0; //counter as we traverse across the inputted word
         char firstVowel = ' '; // placeholder for the first vowel that we run into
         char currentLetter = ' '; // keep track of the current letter that you're comparing
 
+        int i = 0; //counter as we traverse across the inputted word
+
         while(i < input.length()){
-            currentLetter = input.charAt(i); //set current letter check to char at index i
+            currentLetter = input.charAt(i); //set currentLetter to char at index i
+            if(vowels.indexOf(currentLetter) != -1){ //check if the currentLetter is a vowel
+                firstVowel = input.charAt(i); //if vowel, then set as first vowel
 
-
-            i++;
+                System.out.println("The submitted word was " + input + " and the first vowel is: "
+                + firstVowel);
+                break; //exit loop, no longer necessary b/c first vowel found
+            }
+            i++; //iterate to next character
         }
-
-        System.out.println("The submitted word was + " + input + " and the first vowel is ");
-                            + firstVowel);
     }
 }
