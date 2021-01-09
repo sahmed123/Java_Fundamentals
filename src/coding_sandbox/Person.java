46740b100d8629ef20c2cb667c76bf98168591e0
PCR-Admin <sahmed123@hotmail.com>
2021-01-08 19:00:02 -0800

    Classes example

---
+++ src/coding_sandbox/Person.java
@@ -0,0 +1,17 @@
+package coding_sandbox;
+
+public class Person {
+
+    // instance variables - each object of this class type gets a copy
+    int age;
+    double height;
+    String name;
+
+    // constructors
+    // ...
+
+    // methods
+    // ...
+}
+
+//File always has the same name as a the class
