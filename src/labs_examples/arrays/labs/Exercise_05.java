d71b5c36ad007ade18cfdc31315157a79ae3fad3
PCR-Admin <sahmed123@hotmail.com>
2021-01-08 17:27:24 -0800

    WIP

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -6,8 +6,24 @@ package labs_examples.arrays.labs;
  *      Please create and populate an array of your choosing. Then, please demonstrate how to print out every other
  *      element in the array in reverse order.
  *
+ *      My Method
+ *      Create an array of whatever type and then run through a for loop with a limit set for the # of elements
+ *      Printing in reverse order can be done by traversing from end to the start
+ *      Every other element means that you need to track whether the last element was printed or not
+ *      Use a boolean lastPrinted value to flip between yes and no till you iterate through the entire array
+ *
+ *
  */
 
 public class Exercise_05 {
+    public static void main (String[] args){
+
+        String[] animals = {"cat", "dog", "frog", "horse", "fish", "hamster"};
+
+        for(int i = 0; i < animals.length; i++){
+
+        }
+
+    }
 
 }
