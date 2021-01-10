3d25e9da54c227095236b2464a7d629df11aded9
sahmed123 <sahmed123@hotmail.com>
2021-01-09 16:21:23 -0800

    Completed

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,13 +9,32 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
-    public static void main (String[] args){
-        int[][] twoDimsTwoFurious = new int[3][];
-        //Not quite certain of how to populate this, need to look up info on irregular arrays
+    public static void main (String[] args) {
 
-        //for(int[] i: twoDimsTwoFurious)
-          //  for(int[] j: twoDimsTwoFurious)
-            //System.out.println(twoDimsTwoFurious);
+        /*int[][] twoDimsTwoFurious = {
+                {5,6,7,8},
+                {6,7},
+                {9,8,7}
+        }; */
+
+        String[][] twoDimstwoStrings = {
+                {"Hello","This","Is","The","First","Array!"},
+                {"Now","We","Are","In","The","Second!"},
+                {"Number","Three!"}
+        };
+
+        /*for (int[] i : twoDimsTwoFurious) { //first loop checks for each array within 2-d array
+            for (int j : i) { //2nd loop goes through for each element within the array
+                System.out.print(j + " "); //print out each of the elements within the array
+            }
+            System.out.println();
+        }*/
+
+        for (String[] i : twoDimstwoStrings) { //first loop checks for each array within 2-d array
+            for (String j : i) { //2nd loop goes through for each element within the array
+                System.out.print(j + " "); //print out each of the elements within the array
+            }
+            System.out.println();
+        }
     }
-
 }
