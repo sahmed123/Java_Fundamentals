3fbd5e59fcc7f3d2d86d9e4d674bcdbd85bcad41
sahmed123 <sahmed123@hotmail.com>
2021-01-11 23:02:01 -0800

    Beginning Objects Lab

---
+++ src/labs_examples/objects_classes_methods/labs/objects/Airplane.java
@@ -0,0 +1,8 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+/**
+ * Lab Assignment on building an Airplane Object modeled off the CarExample.java
+ */
+
+public class Airplane {
+}
