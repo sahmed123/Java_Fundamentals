3dc2abf04c60e7db18e7ac50129a1206093d8830
PCR-Admin <sahmed123@hotmail.com>
2021-01-13 15:57:37 -0800

    StudentController example

---
+++ src/labs_examples/objects_classes_methods/labs/StudentController/MVC.java
@@ -0,0 +1,26 @@
+package labs_examples.objects_classes_methods.labs.StudentController;
+
+public class MVC {
+    public static void main (String[] args){
+
+        //Fetch student record based upon the roll # in the database
+        Student model = retrieveStudentFromDatabase();
+
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+
+        controller.updateView();
+
+        controller.setStudentName("Shaina");
+
+        controller.updateView();
+    }
+
+    private static Student retrieveStudentFromDatabase(){
+        Student student = new Student();
+        student.setName("Shamim");
+        student.setRollNo("10A");
+        return student;
+    }
+}
---
+++ src/labs_examples/objects_classes_methods/labs/StudentController/Student.java
@@ -0,0 +1,26 @@
+package labs_examples.objects_classes_methods.labs.StudentController;
+
+/**
+ * Create the model for the Student class
+ */
+
+public class Student {
+    private String rollNo;
+    private String name;
+
+    public String getRollNo(){
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo){
+        this.rollNo = rollNo;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
