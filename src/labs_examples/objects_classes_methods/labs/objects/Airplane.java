4cc1d235d43324d4d9f6a1a4300a9769546189bf
PCR-Admin <sahmed123@hotmail.com>
2021-01-12 18:25:47 -0800

    Ex. 1 Complete

--- src/labs_examples/objects_classes_methods/labs/objects/Airplane.java
+++
@@ -1,8 +0,0 @@
-package labs_examples.objects_classes_methods.labs.objects;
-
-/**
- * Lab Assignment on building an Airplane Object modeled off the CarExample.java
- */
-
-public class Airplane {
-}
---
+++ src/labs_examples/objects_classes_methods/labs/objects/AirplaneObject.java
@@ -0,0 +1,57 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+/**
+ * Lab Assignment on building an Airplane Object modeled off the CarExample.java
+ */
+
+public class AirplaneObject {
+    public static void main(String[] args){
+
+        PlaneEngine myEngine = new PlaneEngine(5000);
+        NavigationSystem myNav = new NavigationSystem("Alexa");
+        Manufacturer myModel = new Manufacturer("Boeing");
+
+        Airplane myPlane = new Airplane(myModel, 50000, 45000, myEngine, myNav);
+    }
+}
+
+class Airplane{
+    double fuelCapacity;
+    double currentFuelLevel;
+    PlaneEngine engine;
+    NavigationSystem gps;
+    Manufacturer make;
+
+    public Airplane(Manufacturer make, double fuelCapacity, double currentFuelLevel, PlaneEngine engine, NavigationSystem gps){
+        this.make = make;
+        this.fuelCapacity = fuelCapacity;
+        this.currentFuelLevel = currentFuelLevel;
+        this.engine = engine;
+        this.gps = gps;
+    }
+}
+
+class PlaneEngine{
+    double horsePower;
+
+    public PlaneEngine(int horsePower){
+        this.horsePower = horsePower;
+    }
+
+}
+
+class NavigationSystem{
+    String gps;
+
+    public NavigationSystem(String gps){
+        this.gps = gps;
+    }
+}
+
+class Manufacturer{
+    String manufacturer;
+
+    public Manufacturer(String manufacturer){
+        this.manufacturer = manufacturer;
+    }
+}
